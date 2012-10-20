47876d7b3f0e8b087d63bca21d3185cd5b4a3e9c
Carl Caum <carl@carlcaum.com>
2012-10-19 23:46:01 -0500

    Support node group class membership parameters
    This commit adds the routes, controllers, and views for supporting
    parameters for the NodeGroupClassMembership model. It's fully
    functional, however the parameters are not included in the
    Node#configuration method which means the parameters don't get included
    in the yaml for ENC requests

--- app/models/node_group_class_membership.rb
+++ app/models/node_group_class_membership.rb
@@ -1,6 +1,8 @@
 class NodeGroupClassMembership < ActiveRecord::Base
   validates_presence_of :node_class_id, :node_group_id
 
+  has_parameters
+
   belongs_to :node_class
   belongs_to :node_group
 end
