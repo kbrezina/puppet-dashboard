8cd650069f090255d9db2b663372856c09527228
Karel Brezina <karel.brezina@gmail.com>
2012-11-09 12:28:25 +0100

    An alert is shown for missing parameters. This is just a preparation for the alert that popups on conflicts.

--- app/controllers/node_group_class_memberships_controller.rb
+++ app/controllers/node_group_class_memberships_controller.rb
@@ -5,4 +5,23 @@ class NodeGroupClassMembershipsController < InheritedResources::Base
 
   include SearchableIndex
 
+  def update
+    update! do |success, failure|
+      success.html {
+
+        membership = NodeGroupClassMembership.find_by_node_group_id_and_node_class_id(
+                                                @node_group_class_membership.node_group_id, @node_group_class_membership.node_class_id)
+
+        @node_group_class_membership = membership
+
+        # TODO replace with a real method that checks conflicts
+        @conflicts = @node_group_class_membership.parameters.length == 0
+        if @conflicts
+          render :edit
+       else
+          redirect_to @node_group_class_membership
+        end
+      }
+    end
+  end
 end
