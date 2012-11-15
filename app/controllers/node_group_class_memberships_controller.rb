8b56d25893c7fb9c9557918553ed0b6d1da1e56c
Karel Brezina <karel.brezina@gmail.com>
2012-11-15 15:14:19 +0100

    Check conflicts when a node_group_class_membership is about to be deleted

--- app/controllers/node_group_class_memberships_controller.rb
+++ app/controllers/node_group_class_memberships_controller.rb
@@ -12,13 +12,9 @@ class NodeGroupClassMembershipsController < InheritedResources::Base
 
       update! do |success, failure|
         success.html {
+          membership = NodeGroupClassMembership.find_by_id(params[:id])
 
           unless(force_update?)
-            membership = NodeGroupClassMembership.find_by_node_group_id_and_node_class_id(
-                                                    @node_group_class_membership.node_group_id, @node_group_class_membership.node_class_id)
-
-            @node_group_class_membership = membership
-
             new_conflicts_message = get_new_conflicts_message(old_conflicts)
             unless new_conflicts_message.nil?
               html = render_to_string(:template => "shared/_confirm",
@@ -29,14 +25,43 @@ class NodeGroupClassMembershipsController < InheritedResources::Base
             end
           end
 
-          render :json => { :status => "ok", :valid => "true", :redirect_to => url_for(@node_group_class_membership) }, :content_type => 'application/json'
+          render :json => { :status => "ok", :valid => "true", :redirect_to => url_for(membership) }, :content_type => 'application/json'
         };
 
         failure.html {
+          membership = NodeGroupClassMembership.find_by_id(params[:id])
           html = render_to_string(:template => "shared/_error",
                                   :layout => false,
-                                  :locals => { :object_name => 'node_group_class_membership', :object => @node_group_class_membership })
+                                  :locals => { :object_name => 'node_group_class_membership', :object => membership })
           render :json => { :status => "error", :error_html => html }, :content_type => 'application/json'
+        }
+      end
+    end
+  end
+
+  def destroy
+    membership_node_group = NodeGroupClassMembership.find_by_id(params[:id]).node_group
+
+    ActiveRecord::Base.transaction do
+      old_conflicts = get_all_current_conflicts
+
+      destroy! do |_, format| # only one format is used for destroy (success/failure is not recognized)
+                              # TODO recognize and report failed delete
+        format.html {
+
+          unless(force_delete?)
+            new_conflicts_message = get_new_conflicts_message(old_conflicts)
+
+            unless new_conflicts_message.nil?
+              html = render_to_string(:template => "shared/_confirm",
+                                      :layout => false,
+                                      :locals => { :message => new_conflicts_message, :confirm_label => "Delete", :on_confirm_clicked_script => "eval($('delete_button').getAttribute('onclick').replace('?force_delete=false', '?force_delete=true').replace('return false;', '').replace('confirm(\\'Are you sure?\\')', 'true'));" })
+              render :json => { :status => "ok", :valid => "false", :confirm_html => html }, :content_type => 'application/json'
+              raise ActiveRecord::Rollback
+            end
+          end
+
+          render :json => { :status => "ok", :valid => "true", :redirect_to => url_for(membership_node_group) }, :content_type => 'application/json'
         }
       end
     end
@@ -46,5 +71,9 @@ class NodeGroupClassMembershipsController < InheritedResources::Base
 
   def force_update?
     !params["force_update"].nil? && params["force_update"] == "true"
+  end
+
+  def force_delete?
+    !params["force_delete"].nil? && params["force_delete"] == "true"
   end
 end
